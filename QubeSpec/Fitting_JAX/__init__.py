ec86f2375dd2ecb3876582de212c7cb8e0128066
Jan Scholtz <honzascholtz@gmail.com>
2025-09-28 09:11:06 +0100

    removing jax

--- QubeSpec/Fitting_JAX/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-JAX-based fitting infrastructure for QubeSpec
-
-This module contains JAX-compatible versions of the fitting infrastructure,
-including priors, likelihoods, and BlackJAX integration.
-"""
--- QubeSpec/Models_JAX/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-JAX-based spectral models for QubeSpec
-
-This module contains JAX-compatible versions of all spectral models,
-converted from the original NumPy/Numba implementations.
-"""
