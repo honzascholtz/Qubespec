f52611d24e84930fb95b86aceacda66b8fefa75d
Jan Scholtz <honzascholtz@gmail.com>
2026-08-26 16:32:30 +0100

    adding @jit
    it is to speed up fitting.

--- QubeSpec/Models/Custom_model.py
+++ QubeSpec/Models/Custom_model.py
@@ -1,5 +1,29 @@
 import numpy as np
-from astropy.modeling.powerlaws import PowerLaw1D
+import numba
+
+_CONT_POWER, _CONT_LINEAR, _CONT_NONE = 0, 1, 2
+
+
+@numba.njit
+def _evaluate_general_model(x, pars, rest_wave, z_idx, fwhm_idx, peak_idx, ratio_coef,
+                             cont_type, cont_idx, cont_grad_idx, cont_z_idx, cont_pivot_wave):
+    lines = np.zeros_like(x)
+    for i in range(rest_wave.shape[0]):
+        mu = rest_wave[i]*(1 + pars[z_idx[i]])/1e4
+        sig = pars[fwhm_idx[i]]/3e5*mu/2.35482
+        amp = ratio_coef[i]*pars[peak_idx[i]]
+        lines += amp*np.exp(-((x - mu)**2)/(2*sig*sig))
+
+    if cont_type == _CONT_POWER:
+        pivot = cont_pivot_wave/1e4*(1 + pars[cont_z_idx])
+        cont = pars[cont_idx]*(x/pivot)**(-pars[cont_grad_idx])
+    elif cont_type == _CONT_LINEAR:
+        cont = pars[cont_grad_idx]*x + pars[cont_idx]
+    else:
+        cont = np.zeros_like(x)
+
+    return cont + lines
+
 
 class general_model:
     """
@@ -56,12 +80,33 @@ class general_model:
             raise KeyError(f'priors is missing entries for: {missing}')
         self.priors = priors
 
-    def gauss(self, x, k, mu, FWHM):
-        sig = FWHM/3e5*mu/2.35482
-        expo = -((x-mu)**2)/(2*sig*sig)
-        return k*np.exp(expo)
+        # Flatten components into plain numpy arrays so `model()` can be evaluated
+        # by a numba-jitted function - fast enough to call once per emcee step.
+        idx = {lab: i for i, lab in enumerate(self.labels)}
+        self._rest_wave = np.array([c['wave'] for c in components.values()])
+        self._z_idx = np.array([idx[c['z']] for c in components.values()])
+        self._fwhm_idx = np.array([idx[c['fwhm']] for c in components.values()])
+        self._peak_idx = np.array([
+            idx[components[c['ratio_to']].get('peak_name', f"{c['ratio_to']}_peak")]
+            if 'ratio_to' in c else idx[c.get('peak_name', f'{name}_peak')]
+            for name, c in components.items()
+        ])
+        self._ratio_coef = np.array([c.get('ratio', 1.0) for c in components.values()])
+
+        self._cont_type_code = {'power': _CONT_POWER, 'linear': _CONT_LINEAR, 'none': _CONT_NONE}[self.continuum]
+        self._cont_idx = idx['cont'] if self.continuum != 'none' else -1
+        self._cont_grad_idx = idx['cont_grad'] if self.continuum != 'none' else -1
+        self._cont_z_idx = idx[self.cont_z] if self.continuum == 'power' else -1
 
     def model(self, x, *pars):
+        pars = np.asarray(pars, dtype=np.float64)
+        return _evaluate_general_model(
+            x, pars, self._rest_wave, self._z_idx, self._fwhm_idx, self._peak_idx, self._ratio_coef,
+            self._cont_type_code, self._cont_idx, self._cont_grad_idx, self._cont_z_idx, self.cont_wave,
+        )
+
+    def line_profiles(self, x, *pars):
+        """Slow, diagnostic per-line breakdown (e.g. for plotting) - not used by the fit itself."""
         p = dict(zip(self.labels, pars))
 
         amp = {}
@@ -72,22 +117,13 @@ class general_model:
             if 'ratio_to' in c:
                 amp[name] = c['ratio']*amp[c['ratio_to']]
 
-        self.profiles = {}
-        self.lines = 0.
+        profiles = {}
         for name, c in self.components.items():
             mu = c['wave']*(1 + p[c['z']])/1e4
-            self.profiles[name] = self.gauss(x, amp[name], mu, p[c['fwhm']])
-            self.lines = self.lines + self.profiles[name]
+            sig = p[c['fwhm']]/3e5*mu/2.35482
+            profiles[name] = amp[name]*np.exp(-((x - mu)**2)/(2*sig*sig))
 
-        if self.continuum == 'power':
-            pivot = self.cont_wave/1e4*(1 + p[self.cont_z])
-            self.cont = PowerLaw1D.evaluate(x, p['cont'], pivot, alpha=p['cont_grad'])
-        elif self.continuum == 'linear':
-            self.cont = p['cont_grad']*x + p['cont']
-        else:
-            self.cont = 0.
-
-        return self.cont + self.lines
+        return profiles
 
     
 if __name__ == '__main__':
